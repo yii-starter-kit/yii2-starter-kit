d7cb86d1ae54b35d661945cf4dd587165915d70e
Eugine Terentev <eugine@terentev.net>
2014-08-13 16:07:07 +0300

    fixes,removed unused tests

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,9 @@ $config = [
             'disabledCommands' => ['netmount'], //отключение ненужных команд https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#commands
             'roots' => [
                 [
-                    'path'   => '@storage/uploads',
+                    'baseUrl' => '@storageUrl',
+                    'basePath' => '@storage',
+                    'path'   => '/uploads',
                     'name'   => ['category' => 'app','message' => 'Uploads'], // Yii::t($category, $message)
                     'access' => ['read' => 'manager', 'write' => 'manager'] // * - для всех, иначе проверка доступа в даааном примере все могут видет а редактировать могут пользователи только с правами UserFilesAccess
                 ]

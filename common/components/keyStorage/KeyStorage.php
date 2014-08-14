e80076987b9739cdc1bd66fe11c43c7877cf3029
Eugine Terentev <eugine@terentev.net>
2014-08-14 10:34:22 +0300

    key storage improvements

--- common/components/keyStorage/KeyStorage.php
+++ common/components/keyStorage/KeyStorage.php
@@ -8,20 +8,54 @@
 
 namespace common\components\keyStorage;
 
-use common\models\KeyStorageItem;
 use yii\base\Component;
 
+/**
+ * Class KeyStorage
+ * @package common\components\keyStorage
+ */
 class KeyStorage extends Component{
+    /**
+     * @var string
+     */
     public $cachePrefix = '_keyStorage';
+    /**
+     * @var int
+     */
     public $cachingDuration = 60;
+    /**
+     * @var string
+     */
+    public $modelClass = '\common\models\KeyStorageItem';
 
+    /**
+     * @param $key
+     * @param $value
+     * @return mixed
+     */
     public function set($key, $value){
         $model = $this->getModel($key);
-        if(!$model) return false;
+        if(!$model) $model = new $this->modelClass;
         $model->value = $value;
         return $model->save();
     }
 
+
+    /**
+     * @param array $values
+     */
+    public function setAll(array $values){
+        foreach($values as $key => $value){
+            $this->set($key, $value);
+        }
+    }
+
+    /**
+     * @param $key
+     * @param null $default
+     * @param bool $cache
+     * @return mixed|null
+     */
     public function get($key, $default = null, $cache = true){
         if($cache){
             $cacheKey = sprintf('%s.%s', $this->cachePrefix, $key);
@@ -42,7 +76,38 @@ class KeyStorage extends Component{
         return $value;
     }
 
-    protected function getModel($key){
-        return KeyStorageItem::findOne(['key'=>$key]);
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function remove($key){
+        return call_user_func($this->modelClass.'::deleteAll', ['key'=>$key]);
     }
+
+    /**
+     * @param array $keys
+     */
+    public function removeAll(array $keys){
+        foreach($keys as $key){
+            $this->remove($key);
+        }
+    }
+
+    /**
+     * @param $groupKey
+     * @return mixed
+     */
+    public function removeGroup($groupKey){
+        return call_user_func($this->modelClass.'::findOne', ['like', 'key', $groupKey]);
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    protected function getModel($key){
+        return call_user_func($this->modelClass.'::findOne', ['key'=>$key]);
+    }
+
+
 }

ef3d6235713427608845e301d9002c2ae56816ff
Eugine Terentev <eugine@terentev.net>
2014-08-16 18:57:59 +0300

    security default role fix

--- console/migrations/m140703_123000_user.php
+++ console/migrations/m140703_123000_user.php
@@ -19,9 +19,8 @@ class m140703_123000_user extends Migration
             'password_hash' => Schema::TYPE_STRING . ' NOT NULL',
             'password_reset_token' => Schema::TYPE_STRING,
             'email' => Schema::TYPE_STRING . ' NOT NULL',
-            'role' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
-
-            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
+            'role' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT '.\common\models\User::ROLE_USER,
+            'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT '.\common\models\User::STATUS_ACTIVE,
             'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
             'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
         ], $tableOptions);

f81c6bf55a7955d17905995f8765ea8f70393eea
Eugine Terentev <eugine@terentev.net>
2014-08-16 18:53:15 +0300

    config fixes

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,31 +11,9 @@ $config = [
             'defaultRoles' => ['administrator', 'manager', 'user'],
         ],
 
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'useFileTransport' => true,
-        ],
-
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\DbTarget',
-                    'levels' => ['error', 'warning'],
-                    'except'=>['yii\web\HttpException:404'], // todo: DbTarget для 404 и 403
-                    'logVars'=>[],
-                    'logTable'=>'{{%system_log}}'
-                ]
-            ],
-        ],
-
         'user' => [
             'loginUrl'=>['user/login'],
             'enableAutoLogin' => true,
-        ],
-
-        'errorHandler' => [
-            'errorAction' => 'site/error',
         ],
 
         'request'=>[
@@ -50,19 +28,5 @@ $config = [
         ],
     ],
 ];
-
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
-}
-
-$config = yii\helpers\ArrayHelper::merge(
-    $base,
-    $config
-);
 
 return $config;

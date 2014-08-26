dddfac732b3eb36860193ba1988b296f76b4e613
Eugine Terentev <eugine@terentev.net>
2014-08-26 22:59:07 +0300

    fix

--- backend/views/log/view.php
+++ backend/views/log/view.php
@@ -4,23 +4,13 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
-/* @var $model backend\\models\SystemLog */
+/* @var $model backend\models\SystemLog */
 
 $this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('backend', 'System Logs'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="system-log-view">
-
-    <p>
-        <?= Html::a(Yii::t('backend', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('backend', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
     <?= DetailView::widget([
         'model' => $model,

28516e6a80d56f6c1ca253ae089241da4ae1e822
Eugine Terentev <eugine@terentev.net>
2014-09-06 00:19:04 +0300

    imperavi upload fix

--- backend/controllers/FileManagerController.php
+++ backend/controllers/FileManagerController.php
@@ -7,7 +7,7 @@ class FileManagerController extends \yii\web\Controller
     public function actions(){
         return [
             'upload-imperavi'=>[
-                'class'=>'trntv\yii2-file-kit\actions\UploadAction',
+                'class'=>'trntv\filekit\actions\UploadAction',
                 'fileparam'=>'file',
                 'responseUrlParam'=>'filelink',
                 'disableCsrf'=>true

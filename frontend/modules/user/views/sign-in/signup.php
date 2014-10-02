610a910387e75e2f1cce3dc636727fc35e056e4b
Eugine Terentev <eugine@terentev.net>
2014-10-02 16:53:57 +0300

    storage folder

--- frontend/modules/user/views/sign-in/signup.php
+++ frontend/modules/user/views/sign-in/signup.php
@@ -6,7 +6,7 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 /* @var $model \frontend\modules\user\models\SignupForm */
 
-$this->title = 'Signup';
+$this->title = Yii::t('frontend', 'Signup');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">

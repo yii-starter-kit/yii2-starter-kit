b7df39d65cf636e9305ca46b361bbc69b9d0148c
Eugine Terentev <eugine@terentev.net>
2014-10-18 09:56:58 +0300

    missed files

---
+++ common/migrations/m141007_204254_i18n_message_source.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141007_204254_i18n_message_source extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m141007_204254_i18n_message_source cannot be reverted.\n";
+
+        return false;
+    }
+}
---
+++ common/migrations/m141007_204314_i18n_uk_translation.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141007_204314_i18n_uk_translation extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m141007_204314_i18n_ua_translation cannot be reverted.\n";
+
+        return false;
+    }
+}

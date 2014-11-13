10e579b552752353c2daa829ba4cc0451653584a
Eugine Terentev <eugine@terentev.net>
2014-11-14 00:17:44 +0200

    cleanup

--- frontend/config/_base.php
+++ frontend/config/_base.php
@@ -3,11 +3,6 @@ return [
     'id' => 'frontend',
     'basePath'=>dirname(__DIR__),
     'components' => [
-        'user' => [
-            'class'=>'yii\web\User',
-            'loginUrl'=>'/user/sign-in/login',
-            'enableAutoLogin' => true,
-        ],
         'urlManager'=>require(__DIR__.'/_urlManager.php'),
     ],
 ];

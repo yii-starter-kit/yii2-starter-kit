b6e1ebad8697e37aab47782b2106ab3fc8437367
Eugine Terentev <eugine@terentev.net>
2014-11-28 19:13:00 +0200

    sign up link added

--- frontend/modules/user/views/sign-in/login.php
+++ frontend/modules/user/views/sign-in/login.php
@@ -20,11 +20,14 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
                 <div style="color:#999;margin:1em 0">
                     <?php echo \Yii::t('frontend', 'If you forgot your password you can reset it <a href="{link}">here</a>', [
-                        'linkl'=>yii\helpers\Url::to(['sign-in/request-password-reset'])
+                        'link'=>yii\helpers\Url::to(['sign-in/request-password-reset'])
                     ]) ?>
                 </div>
                 <div class="form-group">
                     <?= Html::submitButton(Yii::t('frontend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                </div>
+                <div class="form-group">
+                    <?php echo Html::a(\Yii::t('frontend', 'Need an account? Sign up.'), ['sign-up']) ?>
                 </div>
                 <h2><?php echo \Yii::t('frontend', 'Log in with')  ?>:</h2>
                 <div class="form-group">

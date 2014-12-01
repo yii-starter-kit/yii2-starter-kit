a45b8517e82bf729bfc7f83101e92327d640e9af
Eugine Terentev <eugine@terentev.net>
2014-12-01 17:00:50 +0200

    custom components autocompletion

---
+++ autocompletion.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Yii bootstrap file.
+ * Used for enhanced IDE code autocompletion.
+ * Note: To avoid "Multiple Implementations" PHPStorm warning and make autocomplete faster
+ * exclude or "Mark as Plain Text" vendor/yiisoft/yii2/Yii.php file
+ */
+class Yii extends \yii\BaseYii
+{
+    /**
+     * @var BaseApplication|WebApplication|ConsoleApplication the application instance
+     */
+    public static $app;
+}
+
+/**
+ * Class BaseApplication
+ * Used for properties that are identical for both WebApplication and ConsoleApplication
+ *
+ * @property \trntv\filekit\storage\FileStorage $fileStorage The auth manager for this application. Null is returned if auth manager is not configured. This property is read-only. Extended component.
+ * @property common\components\keyStorage\KeyStorage $keyStorage The auth manager for this application. Null is returned if auth manager is not configured. This property is read-only. Extended component.
+ */
+abstract class BaseApplication extends yii\base\Application
+{
+}
+
+/**
+ * Class WebApplication
+ * Include only Web application related components here
+ */
+class WebApplication extends yii\web\Application
+{
+}
+
+/**
+ * Class ConsoleApplication
+ * Include only Console application related components here
+ */
+class ConsoleApplication extends yii\console\Application
+{
+}

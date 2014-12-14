438892abce8cb23f6bda33bfcb578daf64ea0abb
Alexey Sharov <www.pismeco@gmail.com>
2014-12-14 21:03:10 +0700

    forgotten class

---
+++ common/models/query/ArticleCategoryQuery.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zein
+ * Date: 7/4/14
+ * Time: 2:31 PM
+ */
+
+namespace common\models\query;
+
+use common\models\Article;
+use common\models\ArticleCategory;
+use yii\db\ActiveQuery;
+
+class ArticleCategoryQuery extends ActiveQuery
+{
+    /**
+     * @return $this
+     */
+    public function active()
+    {
+        $this->andWhere(['status' => ArticleCategory::STATUS_ACTIVE]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function noParents()
+    {
+        $this->andWhere('article_category.parent_id IS NULL');
+
+        return $this;
+    }
+}

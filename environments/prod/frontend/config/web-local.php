3c3d875d3b3bdf058d7b88c81762ceb4afaf4ba3
Eugine Terentev <eugine@terentev.net>
2015-01-11 21:34:36 +0200

    #82

--- environments/prod/frontend/config/web-local.php
+++ environments/prod/frontend/config/web-local.php
@@ -1,30 +1,26 @@
 <?php
-return \yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/_base.php'),
-    [
-        'components'=>[
-            'authClientCollection' => [
-                'class' => 'yii\authclient\Collection',
-                'clients' => [
-                    'github' => [
-                        'class' => 'yii\authclient\clients\GitHub',
-                        'clientId' => 'your-client-id',
-                        'clientSecret' => 'your-client-secret',
-                    ],
-                    'facebook' => [
-                        'class' => 'yii\authclient\clients\Facebook',
-                        'clientId' => 'your-client-id',
-                        'clientSecret' => 'your-client-secret',
-                    ]
+return [
+    'components'=>[
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'github' => [
+                    'class' => 'yii\authclient\clients\GitHub',
+                    'clientId' => 'your-client-id',
+                    'clientSecret' => 'your-client-secret',
                 ],
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => 'your-client-id',
+                    'clientSecret' => 'your-client-secret',
+                ]
             ],
+        ],
 
-            'request' => [
-                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-                'cookieValidationKey' => '',
-            ],
-
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '',
         ],
 
     ]
-);
+];

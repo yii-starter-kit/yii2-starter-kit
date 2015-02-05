83b3b60bd7420ae7a29fdf4a15aa74264e188873
trntv <pass904143>
2015-02-06 01:33:35 +0200

    log migration fix

--- common/migrations/m140703_123055_log.php
+++ common/migrations/m140703_123055_log.php
@@ -1,32 +1,7 @@
 <?php
+require(Yii::getAlias('@yii/log/migrations/m141106_185632_log_init.php'));
 
-use yii\db\Schema;
-use yii\db\Migration;
-
-class m140703_123055_log extends Migration
+class m140703_123055_log extends m141106_185632_log_init
 {
-    public function up()
-    {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
 
-        $this->createTable('{{%system_log}}', [
-            'id' => Schema::TYPE_PK,
-            'level' => Schema::TYPE_INTEGER,
-            'category' => Schema::TYPE_STRING . '(255)',
-            'log_time' => Schema::TYPE_INTEGER . ' NOT NULL',
-            'prefix' => Schema::TYPE_TEXT,
-            'message' => Schema::TYPE_TEXT
-        ], $tableOptions);
-
-        $this->createIndex('idx_log_level', '{{%system_log}}', 'level');
-        $this->createIndex('idx_log_category', '{{%system_log}}', 'category');
-    }
-
-    public function down()
-    {
-        $this->dropTable('{{%system_log}}');
-    }
 }

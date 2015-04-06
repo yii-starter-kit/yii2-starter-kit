0ef3af697cf1350b09039e35871e0a31f30a093a
Eugene Terentev <eugene@terentev.net>
2015-04-06 22:29:25 +0300

    appendTimestamp config + tests gitignore fix

--- common/config/web.php
+++ common/config/web.php
@@ -1,20 +1,21 @@
 <?php
 $config = [
     'components' => [
-        'assetManager'=>[
-            'class'=>'yii\web\AssetManager',
-            'linkAssets'=>true
+        'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'linkAssets' => true,
+            'appendTimestamp' => YII_ENV_DEV
         ]
     ],
-    'as locale'=>[
-        'class'=>'common\components\behaviors\LocaleBehavior'
+    'as locale' => [
+        'class' => 'common\components\behaviors\LocaleBehavior'
     ]
 ];
 
 if (YII_DEBUG) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
-        'class'=>'yii\debug\Module',
+        'class' => 'yii\debug\Module',
         /*'panels'=>[
             'xhprof'=>[
                 'class'=>'\trntv\debug\xhprof\panels\XhprofPanel'

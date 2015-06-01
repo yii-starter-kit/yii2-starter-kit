2890ddd51727961e753f61ce4521b11e2cd6ca3e
Eugene Terentev <eugene@terentev.net>
2015-06-01 13:25:22 +0300

    dependencies update

--- common/env.php
+++ common/env.php
@@ -2,7 +2,8 @@
 /**
  * Setup application environment
  */
-Dotenv::load(__DIR__ . '/..');
+$dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
 
 defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') === 'true');
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');

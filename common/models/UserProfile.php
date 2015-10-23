7d53aa1014c32703d01b4f178391e2baee99eaae
Alexander Volkov <the.purgen@gmail.com>
2015-10-23 21:02:21 +0300

    Update UserProfile.php

--- common/models/UserProfile.php
+++ common/models/UserProfile.php
@@ -57,7 +57,7 @@ class UserProfile extends \yii\db\ActiveRecord
         return [
             [['user_id'], 'required'],
             [['user_id', 'gender'], 'integer'],
-            [['gender'], 'in', 'range'=>[self::GENDER_FEMALE, self::GENDER_MALE]],
+            [['gender'], 'in', 'range'=>[NULL, self::GENDER_FEMALE, self::GENDER_MALE]],
             [['firstname', 'middlename', 'lastname', 'avatar_path', 'avatar_base_url'], 'string', 'max' => 255],
             ['locale', 'default', 'value' => Yii::$app->language],
             ['locale', 'in', 'range' => array_keys(Yii::$app->params['availableLocales'])],

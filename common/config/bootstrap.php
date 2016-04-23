1e725a271af1ef9cda950b76efa0e273527b7c47
Eugene Terentev <eugene@terentev.net>
2016-04-23 10:21:11 +0300

    code comments fix

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,8 +1,12 @@
 <?php
-// Require core files
+/**
+ * Require core files
+ */
 require_once(__DIR__ . '/../helpers.php');
 
-// Path aliases
+/**
+ * Setting path aliases
+ */
 Yii::setAlias('@base', realpath(__DIR__.'/../../'));
 Yii::setAlias('@common', realpath(__DIR__.'/../../common'));
 Yii::setAlias('@frontend', realpath(__DIR__.'/../../frontend'));
@@ -11,7 +15,9 @@ Yii::setAlias('@console', realpath(__DIR__.'/../../console'));
 Yii::setAlias('@storage', realpath(__DIR__.'/../../storage'));
 Yii::setAlias('@tests', realpath(__DIR__.'/../../tests'));
 
-// Url Aliases
+/**
+ * Setting url aliases
+ */
 Yii::setAlias('@frontendUrl', env('FRONTEND_URL'));
 Yii::setAlias('@backendUrl', env('BACKEND_URL'));
 Yii::setAlias('@storageUrl', env('STORAGE_URL'));
--- common/env.php
+++ common/env.php
@@ -1,15 +1,18 @@
 <?php
-    
-// Require core files
+/**
+ * Require helpers
+ */
 require_once(__DIR__ . '/helpers.php');
 
 /**
- * Setup application environment
+ * Load application environment from .env file
  */
 $dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
 $dotenv->load();
 
-// Init application constants
+/**
+ * Init application constants
+ */
 defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
 

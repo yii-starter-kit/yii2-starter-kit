ddd9e4d5309b21af75b4a029bb6459d5dcbf22ba
David J Eddy <me@davidjeddy.com>
2016-10-20 15:02:13 -0400

    UPDATED config to use single domain by default

--- backend/config/web.php
+++ backend/config/web.php
@@ -23,7 +23,8 @@ $config = [
             'errorAction' => 'site/error',
         ],
         'request' => [
-            'cookieValidationKey' => env('BACKEND_COOKIE_VALIDATION_KEY')
+            'cookieValidationKey' => env('BACKEND_COOKIE_VALIDATION_KEY'),
+            'baseUrl' => env('BACKEND_URL')
         ],
         'user' => [
             'class'=>'yii\web\User',

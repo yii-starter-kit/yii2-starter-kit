b0dbf112f9feae2f0cd89080ab1071bf546b3fa1
David J Eddy <me@davidjeddy.com>
2016-10-25 14:37:35 -0400

    UPDATED FE and BE to be more systematic UPDATED generated support files REPLACED `8080` with %TEST_HTTP_PORT%

---
+++ tests/codeception/backend/_support/UnitHelper.php
@@ -0,0 +1,9 @@
+<?php
+namespace tests\codeception\backend;
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class UnitHelper extends \Codeception\Module
+{
+
+}

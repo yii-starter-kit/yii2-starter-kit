248c0bb6cb5de278d215b3f3dc7eeb198938df87
wangqiang <wangqiang@sobey.com>
2016-11-01 16:40:32 +0800

    目标语言还原为en-US。

--- common/config/base.php
+++ common/config/base.php
@@ -4,7 +4,7 @@ $config = [
     'vendorPath'=>dirname(dirname(__DIR__)).'/vendor',
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'sourceLanguage'=>'en-US',
-    'language'=>'zh-CN',
+    'language'=>'en-US',
     'bootstrap' => ['log'],
     'components' => [
 

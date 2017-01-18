7b84357dec9b2898cf3f4e7b0a92b583f3bec58b
Eugene Terentev <eugene@terentev.net>
2017-01-18 17:44:52 +0200

    minor fixes

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -1,16 +1,16 @@
 <?php
 return [
-    'class'=>'yii\web\UrlManager',
-    'enablePrettyUrl'=>true,
-    'showScriptName'=>false,
-    'rules'=> [
+    'class' => 'yii\web\UrlManager',
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'rules' => [
         // Pages
-        ['pattern'=>'page/<slug>', 'route'=>'page/view'],
+        ['pattern' => 'page/<slug>', 'route' => 'page/view'],
 
         // Articles
-        ['pattern'=>'article/index', 'route'=>'article/index'],
-        ['pattern'=>'article/attachment-download', 'route'=>'article/attachment-download'],
-        ['pattern'=>'article/<slug>', 'route'=>'article/view'],
+        ['pattern' => 'article/index', 'route' => 'article/index'],
+        ['pattern' => 'article/attachment-download', 'route' => 'article/attachment-download'],
+        ['pattern' => 'article/<slug>', 'route' => 'article/view'],
 
         // Api
         ['class' => 'yii\rest\UrlRule', 'controller' => 'api/v1/article', 'only' => ['index', 'view', 'options']],

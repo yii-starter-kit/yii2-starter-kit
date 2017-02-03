35ce52984616da357208df76f1a90045a6254bfc
gogl92 <luisarmando1234@gmail.com>
2017-02-03 02:24:12 -0600

    Materialize Update

--- frontend/web/themes/material/layouts/main.php
+++ frontend/web/themes/material/layouts/main.php
@@ -83,6 +83,21 @@ $this->beginContent('@frontend/views/layouts/_clear.php')
         ]
     ]); ?>
     <?php NavBar::end(); ?>
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <h1 class="header center orange-text"><?php echo Html::encode(\Yii::$app->name); ?></h1>
+            <div class="row center">
+                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+            </div>
+            <div class="row center">
+                <a href="http://materializecss.com/getting-started.html" id="download-button"
+                   class="btn-large waves-effect waves-light orange">Get Started</a>
+            </div>
+            <br><br>
+
+        </div>
+    </div>
 
     <div class="container">
         <div class="section">

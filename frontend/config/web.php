5f85e3e0d38cd101de5c927d9abd88259f203206
gogl92 <luisarmando1234@gmail.com>
2017-02-03 01:57:23 -0600

    Added Material Design

--- frontend/config/web.php
+++ frontend/config/web.php
@@ -54,7 +54,7 @@ $config = [
             'theme' => [
                 'class' => 'yii\base\Theme',
                 'pathMap' => [
-                    '@frontend/views' => '@web/themes/material',
+                    '@app/views' => 'themes/material',
                     '@dektrium/user/views' => '@frontend/views/user',
                     '@dektrium/rbac/views' => '@frontend/views/user'
                 ],

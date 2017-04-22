4e6bc7ea2b0299644024b6c023a0c0158a513476
macbook <luisarmando1234@gmail.com>
2017-04-21 22:22:26 -0500

    Updating backend

--- backend/views/layouts/common.php
+++ backend/views/layouts/common.php
@@ -70,7 +70,7 @@ $bundle = BackendAsset::register($this);
                         <!-- User Account: style can be found in dropdown.less -->
                         <li class="dropdown user user-menu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="<?php //echo Yii::$app->user->identity->userProfile->getAvatar($this->assetManager->getAssetUrl($bundle, 'img/anonymous.jpg')) ?>" class="user-image">
+                                <img src="<?= \Yii::$app->user->identity->profile->getAvatarUrl() ?>" class="user-image">
                                 <span><?php //echo Yii::$app->user->identity->username ?> <i class="caret"></i></span>
                             </a>
                             <ul class="dropdown-menu">

d03296a1b6d76460a925566f0583fbcda6664142
David J Eddy <me@davidjeddy.com>
2017-05-28 21:15:20 -0400

    REMOVED packages not related to test update ADDED `composer.lock` per best practices UPDATED ./docs/testing.md w/ assumptions REMOVED non-test related widget include UPDATED front/backend test configurations some more ADDED *Helper classes for backend testing

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -62,10 +62,3 @@ $this->title = Yii::$app->name;
 
     </div>
 </div>
-
-<?php
-echo \davidjeddy\yii2poll\Poll::widget([
-    'pollName'      => 'Do you like PHP?',
-    'answerOptions' => ['Yes', 'No'],
-]);
-?>

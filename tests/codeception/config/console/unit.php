38fe03edeeb2a868a60396c03915e9df82ef6010
David J Eddy <me@davidjeddy.com>
2017-06-10 12:30:25 -0400

    FIXED incorrect file referance

--- tests/codeception/config/console/unit.php
+++ tests/codeception/config/console/unit.php
@@ -7,7 +7,7 @@ return yii\helpers\ArrayHelper::merge(
     require(YII_APP_BASE_PATH . '/common/config/web.php'),
     require(YII_APP_BASE_PATH . '/frontend/config/base.php'),
     require(YII_APP_BASE_PATH . '/frontend/config/web.php'),
-    require(dirname(__DIR__) . '/common/functional.php'),
+    require(dirname(__DIR__) . '/common/unit.php'),
     require(dirname(__DIR__) . '/base.php'),
     require(dirname(__DIR__) . '/console.php'),
     require(dirname(__DIR__) . '/unit.php')

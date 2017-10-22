8d3588508884154676e5c93261b17f9052ede105
Alfred <alfredleo@gmail.com>
2017-10-22 17:11:05 +0500

    Now we have a fresh database. Adding minimal records to fixtures.

---
+++ tests/codeception/common/fixtures/data/rbac_auth_item.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alfred
+ * Date: 20.10.2017
+ * Time: 16:03
+ */
+
+return [
+    [
+        "name" => "administrator",
+        "type" => "1",
+        "description" => "",
+        "rule_name" => null,
+        "data" => "",
+        "created_at" => "1508507788",
+        "updated_at" => "1508507788"
+    ],
+    [
+        "name" => "editOwnModel",
+        "type" => "2",
+        "description" => "",
+        "rule_name" => "ownModelRule",
+        "data" => "",
+        "created_at" => "1508507788",
+        "updated_at" => "1508507788"
+    ],
+    [
+        "name" => "loginToBackend",
+        "type" => "2",
+        "description" => "",
+        "rule_name" => null,
+        "data" => "",
+        "created_at" => "1508507788",
+        "updated_at" => "1508507788"
+    ],
+    [
+        "name" => "manager",
+        "type" => "1",
+        "description" => "",
+        "rule_name" => null,
+        "data" => "",
+        "created_at" => "1508507787",
+        "updated_at" => "1508507787"
+    ],
+    [
+        "name" => "user",
+        "type" => "1",
+        "description" => "",
+        "rule_name" => null,
+        "data" => "",
+        "created_at" => "1508507787",
+        "updated_at" => "1508507787"
+    ]
+];
---
+++ tests/codeception/common/fixtures/data/widget_carousel_item.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    [
+        "id" => "1",
+        "carousel_id" => "1",
+        "base_url" => "http://yii2-starter-kit.dev",
+        "path" => "img/yii2-starter-kit.gif",
+        "type" => "image/gif",
+        "url" => "/",
+        "caption" => "",
+        "status" => "1",
+        "order" => "0",
+        "created_at" => "",
+        "updated_at" => ""
+    ]
+];

cc41066a9ac08050685dff0c92a842eeff24b67c
Alfred <alfredleo@gmail.com>
2017-10-24 17:15:19 +0500

    Some code polishing on app/truncate and app/drop was added.

--- console/controllers/AppController.php
+++ console/controllers/AppController.php
@@ -3,6 +3,7 @@
 namespace console\controllers;
 
 use Yii;
+use yii\base\Module;
 use yii\console\Controller;
 use yii\helpers\Console;
 
@@ -11,6 +12,22 @@ use yii\helpers\Console;
  */
 class AppController extends Controller
 {
+    private static $dbName;
+    private static $db;
+
+    /**
+     * AppController constructor.
+     * @param string $id
+     * @param Module $module
+     * @param array $config
+     */
+    public function __construct($id, Module $module, array $config = [])
+    {
+        self::$db = \Yii::$app->db;
+        self::$dbName = $this->getDsnAttribute(self::$db->dsn, 'dbname');
+        parent::__construct($id, $module, $config);
+    }
+
     public $writablePaths = [
         '@common/runtime',
         '@frontend/runtime',
@@ -40,22 +57,34 @@ class AppController extends Controller
         \Yii::$app->runAction('rbac-migrate/up', ['interactive' => $this->interactive]);
     }
 
+
+    /**
+     * Truncates all tables in the database.
+     */
     public function actionTruncate()
     {
-        $db = \Yii::$app->db;
-        $dbName = $this->getDsnAttribute('dbname', $db->dsn);
-        echo "This will truncate all tables of current database [$dbName].";
-        $command = $db->createCommand('SET FOREIGN_KEY_CHECKS=0')->execute();
-        $command = $db->createCommand("SHOW FULL TABLES WHERE TABLE_TYPE LIKE '%TABLE'");
-        $res = $command->queryAll();
-        foreach ($res as $row) {
-            $rowName = 'Tables_in_' . $dbName;
-            $command = $db->createCommand('TRUNCATE TABLE `' . $row[$rowName] . '`')->execute();
+        if ($this->confirm('This will drop all tables of current database [' . self::$dbName . '].')) {
+            echo 'This will truncate all tables of current database [' . self::$dbName . '].';
+            self::$db->createCommand('SET FOREIGN_KEY_CHECKS=0')->execute();
+            $command = self::$db->createCommand("SHOW FULL TABLES WHERE TABLE_TYPE LIKE '%TABLE'");
+            $res = $command->queryAll();
+            foreach ($res as $row) {
+                $rowName = 'Tables_in_' . self::$dbName;
+                echo 'Truncating table ' . $row[$rowName] . PHP_EOL;
+                self::$db->createCommand()->truncateTable($row[$rowName])->execute();
+            }
+            self::$db->createCommand('SET FOREIGN_KEY_CHECKS=1')->execute();
         }
-        $command = $db->createCommand('SET FOREIGN_KEY_CHECKS=1')->execute();
     }
 
-    private function getDsnAttribute($name, $dsn)
+
+    /**
+     * Parses DNS string to find name of database
+     * @param $name string, string to find
+     * @param $dsn string, DNS string
+     * @return null|string
+     */
+    private function getDsnAttribute($dsn, $name = 'dbname')
     {
         if (preg_match('/' . $name . '=([^;]*)/', $dsn, $match)) {
             return $match[1];
@@ -63,6 +92,24 @@ class AppController extends Controller
             return null;
         }
     }
+
+
+    /**
+     * Drops all tables in the database.
+     */
+    public function actionDrop()
+    {
+        if ($this->confirm('This will drop all tables of current database [' . self::$dbName . '].')) {
+            self::$db->createCommand("SET foreign_key_checks = 0")->execute();
+            $tables = self::$db->schema->getTableNames();
+            foreach ($tables as $table) {
+                echo 'Dropping table ' . $table . PHP_EOL;
+                self::$db->createCommand()->dropTable($table)->execute();
+            }
+            self::$db->createCommand("SET foreign_key_checks = 1")->execute();
+        }
+    }
+
 
     public function actionSetWritable()
     {

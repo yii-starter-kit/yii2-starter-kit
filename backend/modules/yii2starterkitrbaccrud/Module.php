6f50f1f123b8ec3e21e6db9c3f23492d75d362d7
David J Eddy <me@davidjeddy.com>
2017-11-29 10:50:12 -0500

    CREATED yii2-starter-kit rbac crud module, named `yii2starterkitrbaccrud` (y2skrc) ADDED y2skrc to backend ./config/web.php

---
+++ backend/modules/yii2starterkitrbaccrud/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\yii2starterkitrbaccrud;
+
+/**
+ * rbac-crud module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\modules\yii2starterkitrbaccrud\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}

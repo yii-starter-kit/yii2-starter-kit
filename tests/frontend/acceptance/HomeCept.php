0b3dfeab0b0c49367f67a996f0690c6f70b473d4
David J Eddy <me@davidjeddy.com>
2018-05-06 21:16:23 -0400

    FIX syntax error.

--- tests/frontend/acceptance/HomeCept.php
+++ tests/frontend/acceptance/HomeCept.php
@@ -7,7 +7,7 @@ use tests\frontend\AcceptanceTester;
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see(env(APP_NAME);
+$I->see(env('APP_NAME'));
 $I->seeLink('About');
 $I->click('About');
 $I->seeInCurrentUrl('/page/about');

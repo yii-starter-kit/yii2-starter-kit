a91201610bd1c6e1e38b033d0d1de6632344dbcc
petun <petun911@gmail.com>
2018-12-17 16:52:01 +0300

    fix sidebar menu dropdown

--- backend/views/layouts/common.php
+++ backend/views/layouts/common.php
@@ -134,7 +134,7 @@ $bundle = BackendAsset::register($this);
             </div>
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <?php echo Menu::widget([
-                'options' => ['class' => 'sidebar-menu'],
+                'options' => ['class' => 'sidebar-menu', 'data-widget' => 'tree'],
                 'linkTemplate' => '<a href="{url}">{icon}<span>{label}</span>{right-icon}{badge}</a>',
                 'submenuTemplate' => "\n<ul class=\"treeview-menu\">\n{items}\n</ul>\n",
                 'activateParents' => true,

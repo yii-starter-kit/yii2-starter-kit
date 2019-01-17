49e28f1482f9b74192dfcad5102fc983b98dbf05
Victor Gonzalez <victor.gonzalezro@gmail.com>
2019-01-17 19:16:43 -0300

    remove env() call
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,5 +12,5 @@ $dotenv->required('TEST_DB_PASSWORD');
 
 // Set environment
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'test'));
+defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(__DIR__));

c096dfa3be3ec7dda0fcecdc6a1938e91598776d
Victor Gonzalez <victor.gonzalezro@gmail.com>
2019-03-01 21:04:28 -0300

    add assets folder for swagger compatibility
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

---
+++ api/modules/v1/models/definitions/Article.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace api\modules\v1\models\definitions;
+
+/**
+ * @SWG\Definition(required={"title", "body", "category_id"})
+ *
+ * @SWG\Property(property="id", type="integer")
+ * @SWG\Property(property="slug", type="string")
+ * @SWG\Property(property="title", type="string")
+ * @SWG\Property(property="category_id", type="integer")
+ */
+class Article
+{
+    // dummy class for Swagger definitions
+}

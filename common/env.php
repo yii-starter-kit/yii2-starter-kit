e3fff1e6281bd2cc3c4006c60cb8d428715678fe
Victor Gonzalez <victor@vgr.cl>
2021-08-30 16:46:19 -0400

    closes #778, closes #696; migrate to official yii2 docker images and fix some dotenv issues due to new class formats
    Signed-off-by: Victor Gonzalez <victor@vgr.cl>

--- common/env.php
+++ common/env.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . '/helpers.php');
 /**
  * Load application environment from .env file
  */
-$dotenv = \Dotenv\Dotenv::create(dirname(__DIR__));
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 /**
@@ -15,4 +15,3 @@ $dotenv->load();
  */
 defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
-
